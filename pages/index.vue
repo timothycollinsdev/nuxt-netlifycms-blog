cafa54f56f4a15a2a1a28729fa3a89b48ca1ac58
Timothy Collins <timothycollins.sean@gmail.com>
2020-02-02 18:36:56 +0530

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+
+</style>
